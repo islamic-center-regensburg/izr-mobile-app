f0d25604fa0acc1e5ea7d029ff30f0aec7f8a4fc
medaminbl97 <mohamed.amine.bellil@outlook.de>
2026-05-25 19:36:32 +0200

    feat: add gluestack ui and patches

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Button, ButtonText } from "../components/button";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Islamic Center Regensburg</Text>
+      <Button>
+        <ButtonText>Hello</ButtonText>
+      </Button>
     </View>
   );
 }
