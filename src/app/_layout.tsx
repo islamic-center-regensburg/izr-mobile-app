bd4a5856332e008803b005cc25e7e92b658adbcb
medaminbl97 <mohamed.amine.bellil@outlook.de>
2026-06-14 22:23:56 +0200

    feat: add .env.example for API URL configuration; update .gitignore to exclude .env file; add axios and react-query dependencies; integrate TanstackSuery in RootLayout; enhance i18n setup with react-i18next; minor adjustments in app screen components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { GluestackUIProvider } from "../components/gluestack-ui-provider";
 import { I18nSync } from "../components/i18n-sync";
+import TanstackSuery from "../components/tanstack-query";
 import { useThmanyahFonts } from "../hooks/use-thamanyah-fonts";
 
 export default function RootLayout() {
@@ -11,26 +12,28 @@ export default function RootLayout() {
   }
 
   return (
-    <GluestackUIProvider>
-      <I18nSync />
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="(modals)/font-size"
-          options={{
-            presentation: "modal", // iOS sheet, Android slide-up
-            headerShown: false, // or true with a close button
-          }}
-        />
-        <Stack.Screen
-          name="(modals)/language"
-          options={{
-            presentation: "modal", // iOS sheet, Android slide-up
-            headerShown: false, // or true with a close button
-          }}
-        />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </GluestackUIProvider>
+    <TanstackSuery>
+      <GluestackUIProvider>
+        <I18nSync />
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="(modals)/font-size"
+            options={{
+              presentation: "modal", // iOS sheet, Android slide-up
+              headerShown: false, // or true with a close button
+            }}
+          />
+          <Stack.Screen
+            name="(modals)/language"
+            options={{
+              presentation: "modal", // iOS sheet, Android slide-up
+              headerShown: false, // or true with a close button
+            }}
+          />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </GluestackUIProvider>
+    </TanstackSuery>
   );
 }
